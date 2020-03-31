71e54a463ffcedbd2ddb143fb9b58fd85bcda65d
Jaime Leon <jleonhol@gmail.com>
2020-03-31 12:45:47 +0200

    Rename, add styles and modified colors

--- analysis/test/markers.py
+++ analysis/test/markers.py
@@ -3,8 +3,17 @@ import ROOT as r
 markerColors = [
     r.kBlue, 
     r.kRed, 
-    r.kGreen, 
+    r.kGreen+3, 
     r.kMagenta,
     r.kOrange, 
-    r.kBlack, 
+    r.kBlack,
+    r.kViolet+2
 ]
+
+markerTypes = [
+    20, 
+    21, 
+    22
+]
+
+
