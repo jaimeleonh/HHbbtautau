929c03e6ed12d54a4e53764070dc45361526d4c5
Jaime Leon <jleonhol@gmail.com>
2020-04-07 09:30:03 +0200

    Added logscale plots

--- analysis/test/plotTools.py
+++ analysis/test/plotTools.py
@@ -22,7 +22,7 @@ def makePlot(hlist, fileName, plotscaffold):
     res.Close(); del res, resplot
     return
 
-def combinePlots (hlist, legends, plottingStuff, path, savescaffold): 
+def combinePlots (hlist, legends, plottingStuff, path, savescaffold, logy=False): 
     print "Combining list of plots"
     if len(hlist) == 0: raise RuntimeError("Empty list of plots")
     c   = r.TCanvas("c", "c", 800, 800)
@@ -42,7 +42,7 @@ def combinePlots (hlist, legends, plottingStuff, path, savescaffold):
         leg.AddEntry(hlist[iplot], legends[iplot], "PL")
         hlist[iplot].Draw("histosame")
         if ( hlist[iplot].GetMaximum() > maxim ) : 
-          hlist[0].GetYaxis().SetRangeUser(0, hlist[iplot].GetMaximum()*1.5)
+          hlist[0].GetYaxis().SetRangeUser(0.0001, hlist[iplot].GetMaximum()*1.5)
           maxim = hlist[iplot].GetMaximum()
         #hlist[iplot].Draw("P" + (iplot == 0) * "A" + (iplot != 0) * "same")
 
@@ -65,10 +65,15 @@ def combinePlots (hlist, legends, plottingStuff, path, savescaffold):
     secondtext.Draw("same")
 
 
-    #c.SetLogy()
-    c.SaveAs(path + savescaffold + ".png")
-    c.SaveAs(path + savescaffold + ".pdf")
-    c.SaveAs(path + savescaffold + ".root")
+    if logy: c.SetLogy()
+
+    if not logy : c.SaveAs(path + savescaffold + ".png")
+    else : c.SaveAs(path + savescaffold + "_logy.png")
+    if not logy : c.SaveAs(path + savescaffold + ".pdf")
+    else : c.SaveAs(path + savescaffold + "_logy.pdf")
+    #if not logy : c.SaveAs(path + savescaffold + ".root")
+    #else c.SaveAs(path + savescaffold + "_logy.root")
+    
     c.Close(); del c
 
 
